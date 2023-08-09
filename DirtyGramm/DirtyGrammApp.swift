4a1c8e6e01d4bb15a0650ff4f575bee8a325414d
steellson <a.steellson@gmail.com>
2023-08-09 11:14:59 +0300

    Initial Commit

---
+++ DirtyGramm/DirtyGrammApp.swift
@@ -0,0 +1,17 @@
+//
+//  DirtyGrammApp.swift
+//  DirtyGramm
+//
+//  Created by Andrew Steellson on 09.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct DirtyGrammApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
